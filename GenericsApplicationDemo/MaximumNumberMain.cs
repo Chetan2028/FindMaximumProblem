3870d3ec92ac76a75fb44769ddeb743ca1effc93
Chetan2028 <bmchetan2028@gmail.com>
2020-11-04 21:01:38 +0530

    [Chetan] Add . Added to find maximum float number

--- GenericsApplicationDemo/MaximumNumberMain.cs
+++ GenericsApplicationDemo/MaximumNumberMain.cs
@@ -8,6 +8,9 @@ namespace GenericsApplicationDemo
         {
             int intResult = MaximumNumberDemo.MaximumNumber(40, 50, 100);
             Console.WriteLine("Maximum Integer : " + intResult);
+
+            float floatResult = MaximumNumberDemo.MaximumFloatNumber(50.25f, 56.24f, 100.5f);
+            Console.WriteLine("Maximum Integer : " + floatResult);
         }
     }
 }
